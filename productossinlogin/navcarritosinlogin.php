24971e41f7183d0969908c01bc0ae593ce0ac4f1
odellaf13 <odellaf13@gmail.com>
2024-03-08 11:15:06 +0100

    con borrarcarritosinlogin borrado

--- productossinlogin/navcarritosinlogin.php
+++ productossinlogin/navcarritosinlogin.php
@@ -1,15 +1,3 @@
-<?php
-// Inicializar totalcantidad
-$totalcantidad = 0;
-// Contamos la cantidad total de productos en el carrito
-if (isset($_SESSION["carrito"])) {
-    foreach ($_SESSION["carrito"] as $producto) {
-        if (isset($producto["stock"])) {
-            $totalcantidad += $producto["stock"];
-        }
-    }
-}
-?>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #112956;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Mi tienda</a>
@@ -20,9 +8,6 @@ if (isset($_SESSION["carrito"])) {
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modalcarrito" style="color: red; cursor: pointer;"><i class="bi bi-cart-plus-fill"></i>
-                    <?php
-                        echo "($totalcantidad)"; // Mostrar la cantidad total de productos en el carrito
-                        ?>
                 </a>
                 </li>
             </ul>
