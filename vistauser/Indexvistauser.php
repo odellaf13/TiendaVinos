ba09d0da268909b2e378b7e9399c2d070d5552f8
odellaf13 <odellaf13@gmail.com>
2024-03-11 11:04:07 +0100

    commit si stock es 0 en carrito

--- vistauser/Indexvistauser.php
+++ vistauser/Indexvistauser.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Vista del usuario</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vista del usuario</title>
     <link rel="stylesheet" type="text/css" href="estilovistauser.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
 <?php
@@ -76,55 +76,76 @@ if(isset($_SESSION['username'])) {
     }
 </style>
 
-<div class="center mt-5">
-    <div class="card pt-3">
-        <p style="font-weight: bold; color: #0F6BB7; font-size: 22px;">Carrito de la compra</p>
-        <div class="container-fluid p-2" style="background-color: ghostwhite;">
+<body>
+    <div class="center mt-5">
+        <div class="card pt-3">
+            <p style="font-weight: bold; color: #0F6BB7; font-size: 22px;">Carrito de la compra</p>
+            <div class="container-fluid p-2" style="background-color: ghostwhite;">
 
-            <?php
-            $busqueda = mysqli_query($conexion, "SELECT * FROM producto ");
-            $numero = mysqli_num_rows($busqueda);
-            ?>
+                <?php
+                $busqueda = mysqli_query($conexion, "SELECT * FROM producto ");
+                $numero = mysqli_num_rows($busqueda);
+                ?>
 
-            <h5 class="card-tittle">Resultados (<?php echo $numero; ?>)</h5>
-              <!--agregamos 3 productos por línea-->
-            <div class="row custom-row">
-                <?php while ($resultado = mysqli_fetch_assoc($busqueda)) { ?>
-                    <div class="col mb-4 custom-card">
-                        <form id="formulariocarrito" method="POST" action="carrito.php" onsubmit="alert('Producto añadido al carrito');" enctype="multipart/form-data">
-                            <div class="card">
-                                <img src="<?php echo $resultado["url_imagen"]; ?>" class="card-img-top" style="max-width: 100%; max-height: 150px; object-fit: contain;">
-                                <div class="card-body">
-                                    <a class="category" style="color: #008080; font-size: 1.2em; font-weight: bold;">
-                                        <?php echo $resultado["pvp"]; ?> €
-                                    </a>
-                                    <h5 class="card-title"><?php echo $resultado["nombre"]; ?>
-                                    <small class="text-muted"><strong>(<?php echo $resultado["do"]; ?>)</strong>
-                                    </small>                              
-                                    </h5>
-                                    <p class="card-text" style="max-height: 120px; overflow: hidden;"><?php echo $resultado["descripcion"]; ?></p>
-                                    <button class="btn btn-primary" type="submit"><i class="bi bi-cart-plus-fill"></i>Añadir al carrito</button>
+                <h5 class="card-tittle">Resultados (<?php echo $numero; ?>)</h5>
+                <!--agregamos 3 productos por línea-->
+                <div class="row custom-row">
+                    <?php while ($resultado = mysqli_fetch_assoc($busqueda)) { ?>
+                        <div class="col mb-4 custom-card">
+                            <form id="formulariocarrito" method="POST" action="carrito.php" onsubmit="alert('Producto añadido al carrito');" enctype="multipart/form-data">
+                                <div class="card">
+                                    <img src="<?php echo $resultado["url_imagen"]; ?>" class="card-img-top" style="max-width: 100%; max-height: 150px; object-fit: contain;">
+                                    <div class="card-body">
+                                        <a class="category" style="color: #008080; font-size: 1.2em; font-weight: bold;">
+                                            <?php echo $resultado["pvp"]; ?> €
+                                        </a>
+                                        <h5 class="card-title"><?php echo $resultado["nombre"]; ?>
+                                            <small class="text-muted"><strong>(<?php echo $resultado["do"]; ?>)</strong>
+                                            </small>                              
+                                        </h5>
+                                        <p class="card-text" style="max-height: 120px; overflow: hidden;"><?php echo $resultado["descripcion"]; ?></p>
+                                        <!--creamos un botón donde nada más clickar, hagamos que se verifique un formulario del stock, por si es 0 -->
+                                        <button class="btn btn-primary" type="submit" onclick="return validarFormulario(<?php echo $resultado['stock']; ?>);">
+                                        <i class="bi bi-cart-plus-fill"></i>Añadir al carrito
+                                            </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <input name="producto_id" type="hidden" value="<?php echo $resultado["producto_id"]; ?>" />
-          
-                            <div class="text-content">
                                 <input name="producto_id" type="hidden" value="<?php echo $resultado["producto_id"]; ?>" />
-                                <input name="nombre" type="hidden" value="<?php echo $resultado["nombre"]; ?>" />
-                                <input name="pvp" type="hidden" value="<?php echo $resultado["pvp"]; ?>" />
-                                <input name="stock" type="hidden" value="<?php echo $resultado["stock"]; ?>" />
-                                <input name="do" type="hidden" value="<?php echo $resultado["do"]; ?>" />
-                                <input name="descripcion" type="hidden" value="<?php echo $resultado["descripcion"]; ?>" />
-                                <input name="url_imagen" type="hidden" value="<?php echo $resultado["url_imagen"]; ?>" />
-                                <input name="cantidad" type="number" value="1" class="pl-2" min="1" required />
-                            
-                            </div>
-                        </form>
-                    </div>
-                <?php } ?>
+              
+                                <div class="text-content">
+                                    <input name="producto_id" type="hidden" value="<?php echo $resultado["producto_id"]; ?>" />
+                                    <input name="nombre" type="hidden" value="<?php echo $resultado["nombre"]; ?>" />
+                                    <input name="pvp" type="hidden" value="<?php echo $resultado["pvp"]; ?>" />
+                                    <input name="stock" type="hidden" value="<?php echo $resultado["stock"]; ?>" />
+                                    <input name="do" type="hidden" value="<?php echo $resultado["do"]; ?>" />
+                                    <input name="descripcion" type="hidden" value="<?php echo $resultado["descripcion"]; ?>" />
+                                    <input name="url_imagen" type="hidden" value="<?php echo $resultado["url_imagen"]; ?>" />
+                                    <input name="cantidad" class="cantidadInput" type="number" value="1" class="pl-2" min="1" required />
+                                
+                                </div>
+                            </form>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+    <script>
+    function validarFormulario(stockDisponible) {
+        //vemos la cantidad seleccionada
+        var cantidadSeleccionada = parseInt(document.querySelector('.cantidadInput').value);
+
+        //Hacemos con un if un cálculo de si la cantidad seleccionada es mayor al stock disponible (que será 0, si no hay productos)
+        if (cantidadSeleccionada > stockDisponible) {
+            alert('Lo sentimos. No hay stock disponible del producto');
+            return false;
+        }
+
+        return true; //Si es verdadero, que es > a 0, se envía todo a carrito.php
+    }
+</script>
+
 </body>
+
 </html>
--- vistauser/carrito.php
+++ vistauser/carrito.php
@@ -5,13 +5,12 @@ ini_set('display_errors', '1');
 
 include "../Conexion.php";
 
-// Creamos el carrito si no existe o está vacío
+//Creamos el carrito si no existe o si está vacío
 if (!isset($_SESSION["carrito"]) || empty($_SESSION["carrito"])) {
     $_SESSION["carrito"] = array();
 }
-
-// Empezamos el carrito
-if (isset($_POST["producto_id"])) {
+//Comenzamos el carrito y nos aseguramos que recibimos todos los datos del formulario
+if (isset($_POST["producto_id"], $_POST["nombre"], $_POST["pvp"], $_POST["stock"], $_POST["do"], $_POST["descripcion"], $_POST["url_imagen"], $_POST["cantidad"])) {
     $producto_id = $_POST["producto_id"];
     $nombre = $_POST["nombre"];
     $pvp = $_POST["pvp"];
@@ -21,111 +20,109 @@ if (isset($_POST["producto_id"])) {
     $url_imagen = $_POST["url_imagen"];
     $cantidad = $_POST["cantidad"];
 
-    // Verificamos si el producto ya está en el carrito
+    //nos aseguramos que cada producto está en el carrito
     $encontrado = false;
-    foreach ($_SESSION["carrito"] as $producto) {
+    foreach ($_SESSION["carrito"] as &$producto) {
         if ($producto["producto_id"] == $producto_id) {
-            $producto["stock"] += $cantidad;
+            $nuevoStock = $producto["stock"] + $cantidad;
+            //vemos si hay stock suficiente
+            if ($nuevoStock <= 0) {
+                //No hay suficiente stock disponible
+                echo "No hay stock disponible del producto seleccionado.";
+                exit();
+            }
+            $producto["stock"] = $nuevoStock;
             $encontrado = true;
             break;
         }
     }
-
-    // Si no está en el carrito, lo añadimos
+    //Si no esta el stock en el carrito, lo añadimos
     if (!$encontrado) {
-        $_SESSION["carrito"][] = array(
-            "producto_id" => $producto_id,
-            "nombre" => $nombre,
-            "pvp" => $pvp,
-            "stock" => $cantidad,
-            "do" => $do,
-            "descripcion" => $descripcion,
-            "url_imagen" => $url_imagen
-        );
+        if ($cantidad > 0 && $cantidad <= $stock) {
+            $_SESSION["carrito"][] = array(
+                "producto_id" => $producto_id,
+                "nombre" => $nombre,
+                "pvp" => $pvp,
+                "stock" => $cantidad,
+                "do" => $do,
+                "descripcion" => $descripcion,
+                "url_imagen" => $url_imagen
+            );
+        } else {
+            // No hay suficiente stock disponible, mostrar mensaje y salir
+            echo "No hay stock disponible del producto seleccionado.";
+            exit();
+        }
     }
-
-    // Restar la cantidad al stock en la base de datos
+    //Restamos la cantidad seleccionada por cliente al stock en la base de datos
     $query = "UPDATE producto SET stock = stock - $cantidad WHERE producto_id = $producto_id";
     mysqli_query($conexion, $query);
-
-    // Imprimir el valor de $stock para verificar
-    echo "Stock a restar: $stock";
-
-    // Insertar el pedido en la base de datos
+    //Se inserta el pedido en la base de datos
     $username = $_SESSION['username'];
-
-    // Consulta SQL para obtener el usuario_id basado en el nombre de usuario
+    //pedimos el id de usuario con una consulta
     $consultaUsuario = mysqli_query($conexion, "SELECT usuario_id FROM usuario WHERE username = '$username'");
     $filaUsuario = mysqli_fetch_assoc($consultaUsuario);
 
-    // Verificar si se encontró el usuario y obtener el usuario_id
+    //verificamos si hemos obtenido el id
     if ($filaUsuario) {
         $usuario_id = $filaUsuario['usuario_id'];
         $fecha = date('Y-m-d H:i:s');
-
-        // Calcular el total sumando los productos en el carrito
+        //Calculamos  el total de los productos del carrito
         $total = 0;
         foreach ($_SESSION["carrito"] as $producto) {
             $total += $producto["pvp"] * $producto["cantidad"];
         }
-
-        // Insertar el pedido en la tabla 'pedido'
+        //Inserta el pedido en la tabla pedido de la BBDD
         $query = "INSERT INTO pedido (fecha, total, fk_usuario) VALUES ('$fecha', $total, $usuario_id)";
         mysqli_query($conexion, $query);
 
-        // Obtener el id del pedido recién insertado
+        //Llamamos a pedido_id para hacer linea_pedido
         $pedido_id = mysqli_insert_id($conexion);
 
-        // Insertar las líneas de pedido en la tabla 'linea_pedido'
-      // Insertar las líneas de pedido en la tabla 'linea_pedido'
-foreach ($_SESSION["carrito"] as $producto) {
-    $producto_id = $producto["producto_id"];
-    $cantidad = $producto["stock"];
+        //insertamos pedido en linea_pedido
+        foreach ($_SESSION["carrito"] as $producto) {
+            $producto_id = $producto["producto_id"];
+            $cantidad = $producto["stock"];
 
-    // Verificar si ya existe una línea de pedido para este producto
-    $consultaExistencia = mysqli_query($conexion, "SELECT * FROM linea_pedido WHERE fk_pedido = $pedido_id AND fk_producto = $producto_id");
-    $filaExistencia = mysqli_fetch_assoc($consultaExistencia);
+            //consulta para ver si existe linea_pedido
+            $consultaExistencia = mysqli_query($conexion, "SELECT * FROM linea_pedido WHERE fk_pedido = $pedido_id AND fk_producto = $producto_id");
+            $filaExistencia = mysqli_fetch_assoc($consultaExistencia);
 
-    if ($filaExistencia) {
-        // Si existe, actualiza la cantidad
-        $cantidad += $filaExistencia["cantidad"];
-        $query = "UPDATE linea_pedido SET cantidad = $cantidad WHERE fk_pedido = $pedido_id AND fk_producto = $producto_id";
-    } else {
-        // Si no existe, inserta una nueva línea de pedido
-        $query = "INSERT INTO linea_pedido (fk_pedido, fk_producto, cantidad) VALUES ($pedido_id, $producto_id, $cantidad)";
-    }
+            if ($filaExistencia) {
+                //Si existe, se actualiza la cantidad a la catual
+                $cantidad += $filaExistencia["cantidad"];
+                $query = "UPDATE linea_pedido SET cantidad = $cantidad WHERE fk_pedido = $pedido_id AND fk_producto = $producto_id";
+            } else {
+                //Y si no existe, insertamos una nueva línea de pedido
+                $query = "INSERT INTO linea_pedido (fk_pedido, fk_producto, cantidad) VALUES ($pedido_id, $producto_id, $cantidad)";
+            }
+            mysqli_query($conexion, $query);
+        }
 
-    mysqli_query($conexion, $query);
-}
+//Recalculamos el total con la información de la tabla linea_pedido
+        $total = 0;
+        $consultaLineasPedido = mysqli_query($conexion, "SELECT * FROM linea_pedido WHERE fk_pedido = $pedido_id");
+        while ($lineaPedido = mysqli_fetch_assoc($consultaLineasPedido)) {
+            $producto_id = $lineaPedido["fk_producto"];
+            $cantidad = $lineaPedido["cantidad"];
+            $consultaProducto = mysqli_query($conexion, "SELECT pvp FROM producto WHERE producto_id = $producto_id");
+            $producto = mysqli_fetch_assoc($consultaProducto);
+            $total += $producto["pvp"] * $cantidad;
+        }
+        //Actualizamos el total en pedido
+        mysqli_query($conexion, "UPDATE pedido SET total = $total WHERE pedido_id = $pedido_id");
 
-// Recalcular el total utilizando la información de la tabla 'linea_pedido'
-$total = 0;
-$consultaLineasPedido = mysqli_query($conexion, "SELECT * FROM linea_pedido WHERE fk_pedido = $pedido_id");
-while ($lineaPedido = mysqli_fetch_assoc($consultaLineasPedido)) {
-    $producto_id = $lineaPedido["fk_producto"];
-    $cantidad = $lineaPedido["cantidad"];
-    $consultaProducto = mysqli_query($conexion, "SELECT pvp FROM producto WHERE producto_id = $producto_id");
-    $producto = mysqli_fetch_assoc($consultaProducto);
-    $total += $producto["pvp"] * $cantidad;
-}
-
-// Actualizar el total en la tabla 'pedido'
-mysqli_query($conexion, "UPDATE pedido SET total = $total WHERE pedido_id = $pedido_id");
-
-        // Restablecer el carrito después de completar la compra
-      
-
-        // Redireccionar a la página del carrito
         header("Location: /TiendaVinos/vistauser/Indexvistauser.php");
         exit();
     } else {
-        // Manejar el caso en el que no se pudo obtener el usuario_id
+    //Caso en el que no se pudo obtener el usuario_id
         echo "Error al obtener el usuario_id.";
     }
 } else {
-    // Manejar el caso en el que no se envió información del producto
+    //Si no se envió información del producto
     echo "Error: No se ha proporcionado información del producto.";
 }
 echo "Contenido de la variable de sesión después de actualizar: ";
 print_r($_SESSION["carrito"]);
 ?>
+
